0af411eaa46986593daf45b3b4248cfd855a6a14
Theflygamer <70364207+Theflygamer@users.noreply.github.com>
2020-09-13 22:24:16 +0200

    sql

---
+++ SQL/SQLdone.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Kundeid]
+      ,[Fnavn]
+      ,[Enavn]
+      ,[Alder]
+      ,[Mobilnummer]
+      ,[Email]
+  FROM [Biograf].[dbo].[Kunder]
